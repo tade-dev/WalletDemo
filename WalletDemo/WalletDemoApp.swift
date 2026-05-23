9b55e60b459169f944e6d5000ac8ec9082f6a42e
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-05-23 09:22:33 +0100

    Initial Commit

---
+++ WalletDemo/WalletDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletDemoApp.swift
+//  WalletDemo
+//
+//  Created by BSTAR on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WalletDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
